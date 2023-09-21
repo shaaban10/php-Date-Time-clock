d117ca28bd017d8571f43e90cef49b82818727c2
shaaban alahmed <shaabanalahmed20@gmail.com>
2023-09-21 15:41:15 +0200

    last update

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php Clock</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <h1>php clock</h1>
+    <h1> <span>p</span>h<span>p</span> c<span>l</span>o<span>c</span>k 🕐 </h1>
+    <p><?php echo date("h:i:s A") ;?></p>
 </body>
 </html>
